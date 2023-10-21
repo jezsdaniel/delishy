dd1f107bd32f8812684620419d8766b88b198914
jezsdaniel <jadendaniel7@gmail.com>
2023-10-21 19:17:56 -0400

    add bottom navigation bar on home

--- lib/features/home/presentation/pages/home/page.dart
+++ lib/features/home/presentation/pages/home/page.dart
@@ -1,4 +1,6 @@
+import 'package:delishy/l10n/l10n.dart';
 import 'package:flutter/material.dart';
+import 'package:ionicons/ionicons.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -9,11 +11,49 @@ class HomePage extends StatelessWidget {
   }
 }
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   const HomeView({super.key});
 
   @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  int currentPageIndex = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    final l10n = context.l10n;
+    return Scaffold(
+      body: const Center(
+        child: Text('Home'),
+      ),
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: currentPageIndex,
+        onDestinationSelected: (index) {
+          setState(() {
+            currentPageIndex = index;
+          });
+        },
+        destinations: [
+          NavigationDestination(
+            icon: const Icon(Ionicons.bulb_outline),
+            label: l10n.recipes,
+          ),
+          NavigationDestination(
+            icon: const Icon(Ionicons.list_outline),
+            label: l10n.favorites,
+          ),
+          NavigationDestination(
+            icon: const Icon(Ionicons.restaurant_outline),
+            label: l10n.cookNow,
+          ),
+          NavigationDestination(
+            icon: const Icon(Ionicons.settings_outline),
+            label: l10n.settings,
+          ),
+        ],
+      ),
+    );
   }
 }
