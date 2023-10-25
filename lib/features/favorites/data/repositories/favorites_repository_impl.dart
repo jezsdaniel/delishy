b4ba9967917f83cc3bb9e2dbecc0807e4739d58f
jezsdaniel <jadendaniel7@gmail.com>
2023-10-25 02:19:57 -0400

    add favorites repository

---
+++ lib/features/favorites/data/repositories/favorites_repository_impl.dart
@@ -0,0 +1,46 @@
+import 'package:dartz/dartz.dart';
+import 'package:delishy/core/errors/failure.dart';
+import 'package:delishy/features/favorites/data/data_sources/favorites_local_api.dart';
+import 'package:delishy/features/favorites/domain/repositories/favorites_repository.dart';
+import 'package:delishy/features/recipes/domain/entities/meal.dart';
+
+class FavoritesRepositoryImpl implements FavoritesRepository {
+  FavoritesRepositoryImpl(
+    this.api,
+  );
+
+  final FavoritesLocalStorage api;
+
+  @override
+  Future<Either<Failure, int>> addFavorite(Meal meal) async {
+    try {
+      final resp = await api.addFavorite(meal);
+
+      return Right(resp);
+    } catch (ex) {
+      return Left(Failure());
+    }
+  }
+
+  @override
+  Future<Either<Failure, int>> removeFavorites(Meal meal) async {
+    try {
+      final resp = await api.removeFavorite(meal);
+
+      return Right(resp);
+    } catch (ex) {
+      return Left(Failure());
+    }
+  }
+
+  @override
+  Future<Either<Failure, List<Meal>>> getAllFavorites() async {
+    try {
+      final resp = await api.getAllFavorites();
+
+      return Right(resp);
+    } catch (ex) {
+      return Left(Failure());
+    }
+  }
+}
