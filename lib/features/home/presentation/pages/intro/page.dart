f03bea453a3fdc96ac9898668eb208dbb6997cf8
jezsdaniel <jadendaniel7@gmail.com>
2023-10-25 23:35:13 -0400

    add animations to intro page

--- lib/features/home/presentation/pages/intro/page.dart
+++ lib/features/home/presentation/pages/intro/page.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:delishy/core/constants/assets.dart';
 import 'package:delishy/core/router/routes.dart';
 import 'package:delishy/l10n/l10n.dart';
@@ -25,34 +26,42 @@ class IntroView extends StatelessWidget {
           padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 32),
           child: Column(
             children: [
-              Text(
-                l10n.appName,
-                style: Theme.of(context).textTheme.titleMedium,
-                key: const Key('introView_title'),
+              FadeIn(
+                child: Text(
+                  l10n.appName,
+                  style: Theme.of(context).textTheme.titleMedium,
+                  key: const Key('introView_title'),
+                ),
               ),
               const Spacer(),
-              Image.asset(
-                AssetsPaths.cuttingBoardImage,
-                height: 192,
+              FadeInDown(
+                child: Image.asset(
+                  AssetsPaths.cuttingBoardImage,
+                  height: 192,
+                ),
               ),
               const SizedBox(
                 width: double.infinity,
                 height: 16,
               ),
-              Text(
-                l10n.allTheRecipesOnYourFingertips,
-                style: Theme.of(context).textTheme.titleLarge,
-                textAlign: TextAlign.center,
-                key: const Key('introView_description'),
+              FadeIn(
+                child: Text(
+                  l10n.allTheRecipesOnYourFingertips,
+                  style: Theme.of(context).textTheme.titleLarge,
+                  textAlign: TextAlign.center,
+                  key: const Key('introView_description'),
+                ),
               ),
               const Spacer(),
-              SizedBox(
-                width: double.infinity,
-                child: FilledButton.tonal(
-                  onPressed: () {
-                    context.go(Routes.recipes);
-                  },
-                  child: Text(l10n.letsStart),
+              FadeIn(
+                child: SizedBox(
+                  width: double.infinity,
+                  child: FilledButton.tonal(
+                    onPressed: () {
+                      context.go(Routes.recipes);
+                    },
+                    child: Text(l10n.letsStart),
+                  ),
                 ),
               ),
             ],
